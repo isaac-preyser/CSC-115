5f923a59cf99c2910da48b60536c980e1b372f90
Isaac Preyser <isaacpreyser@icloud.com>
2024-01-10 03:14:17 +0000

    we learned how to import stuff in java again

--- Introductions/HelloWorld.java
+++ Introductions/HelloWorld.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        // Prints "Hello World!" to the terminal window.
+        //accept input from console
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
+        
     }
 }
