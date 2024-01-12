97c92d7d9fc7fbe1dd8581773f5bb5aa16331e08
Isaac Preyser <isaacpreyser@icloud.com>
2024-01-12 23:21:47 +0000

    Update function to handle edge cases
    This commit updates the function to handle edge cases, as shown in the git diff.

---
+++ Module-2/Song.java
@@ -0,0 +1,81 @@
+// Song.java
+
+public class Song {
+	// 1. generalize what you use to describe a song?
+	//    these dictate the fields/attributes in our Song class
+	/*	Song Attributes
+	 *  string: title * we will use
+	 *  string: artist * we will use
+	 *  int: yearReleased * we will use
+	 *  int: durationInSeconds 
+	 *  string: genre
+	 *  string: album * we will use
+	 *  string: recordLabel
+	 *  string: producer
+	 *  string: writer
+	 *  string: composer
+	 */
+	// 2. create fields in Song class
+	public String title;
+	public String artist;
+	public int yearReleased;
+	public String album;
+	// 3. go to tester and create some song objects...
+
+	// 4. change fields to private in Song class
+	//  - what happens when you try to access them from tester?
+	// unable to access private fields from outside the class... 
+	//  - what happens when you try to access them from Song class?
+	// able to access private fields from inside the class...
+
+	// 5. add getters and setters for fields
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getArtist(){
+		return artist;
+	}
+	public void setArtist(String artist){
+		this.artist = artist;
+	}
+	public int getYearReleased(){
+		return yearReleased;
+	}
+	public void setYearReleased(int yearReleased){
+		this.yearReleased = yearReleased;
+	}
+	public String getAlbum(){
+		return album;
+	}
+	public void setAlbum(String album){
+		this.album = album;
+	}
+
+	// 6. how do we initialize a song with data upon creation?
+	//  - add a custom constructor
+	public Song createSong(String title, String artist, int yearReleased, String album){
+		Song song = new Song();
+		song.setTitle(title);
+		song.setArtist(artist);
+		song.setYearReleased(yearReleased);
+		song.setAlbum(album);
+		return song;
+	}
+
+	// 7. add a toString method
+	//  - takes no arguments and returns a String
+	//    representing this object
+	public String toString(){
+		String output = "";
+		output += "Title: " + this.title + "\n";
+		output += "Artist: " + this.artist + "\n";
+		output += "Year Released: " + this.yearReleased + "\n";
+		output += "Album: " + this.album + "\n";
+		return output;
+	}
+	
+   
+}
---
+++ Module-2/SongTester.java
@@ -0,0 +1,71 @@
+// SongTester.java
+//
+// We will write code here to test our implementation of Song.java
+//
+// We have put this code in a different file to help you understand 
+// the difference between static and non-static.  In the real world,
+// this style of test code might be included in the Song.java file.
+
+public class SongTester {
+    
+	public static void main (String[] args) {
+		/* Recall: if I want to declare an integer: */
+		int i; // what is it initialized to? ANSWER: 0
+
+		/* if I want to declare an array of integers: */
+		int[] array;
+		
+		/* what is it initialized to? ANSWER: null
+		   how would I initialize it to point to an array of size 10? */
+		//ANSWER: array = new int[10];
+		/* 3a. declare and initialize two new blank songs */
+		//Song s1 = new Song();
+		//Song s2 = new Song();
+		//lets do this but with the constructor.
+		Song s1 = new Song();
+		Song s2 = new Song();
+		s1 = s1.createSong("Song 1", "Artist 1", 2000, "Album 1");
+		s2 = s2.createSong("Song 2", "Artist 2", 2001, "Album 2");
+		
+
+		/* 3b. print the values of the fields for each song */
+		System.out.println("Song 1: ");
+		System.out.println("Title: " + s1.getTitle());
+		System.out.println("Artist: " + s1.getArtist());
+		System.out.println("Year Released: " + s1.getYearReleased());
+		System.out.println("Album: " + s1.getAlbum());
+		System.out.println();
+		System.out.println("Song 2: ");
+		System.out.println("Title: " + s2.getTitle());
+		System.out.println("Artist: " + s2.getArtist());
+		System.out.println("Year Released: " + s2.getYearReleased());
+		System.out.println("Album: " + s2.getAlbum());
+		System.out.println();
+
+
+
+		/* 3c. set the fields in the song to some values and reprint */
+
+
+
+
+		/* 5a. update statements where necessary from 3b and 3c */
+
+		/* 6a. call constructor with args, then print fields */
+
+
+
+
+		/* 6b. call alternative constructor, then print fields */
+
+
+
+
+
+		/* 6c. try to print one of the songs
+		   ie. pass your Song variable to System.out.println() */
+
+
+		/* 7a. recompile and run your program - what changes in 6c. */
+	}
+}
