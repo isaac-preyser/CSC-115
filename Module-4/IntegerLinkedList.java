83e4a821177065117c7b1c49a8ad0853a7f464fa
Isaac Preyser <isaacpreyser@icloud.com>
2024-01-31 15:00:26 -0800

    Add files via upload

---
+++ Module-4/IntegerLinkedList.java
@@ -0,0 +1,42 @@
+public class IntegerLinkedList implements IntegerList {
+    
+	private Node head;
+	private int numElements;
+
+	public IntegerLinkedList() {
+		head = null;
+		numElements = 0;
+	}
+
+	public void addFront (int val) {
+
+	}
+
+	public void addBack (int val) {
+		
+	}
+
+	public int size (){
+		return 0;
+	}
+
+	public int get (int position) {
+		return 0;
+	}
+	
+	public void insertAt (int position, int val) {
+		
+	}
+
+	/* Purpose: create a string representation of list 
+	 * Parameters: none
+	 * Returns: String - the string representation
+	 */
+	public String toString() {
+		String s = "List contents: ";
+		return s;
+	}    
+}
+
+
+
