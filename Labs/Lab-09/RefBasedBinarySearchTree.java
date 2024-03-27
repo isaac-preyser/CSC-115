a4ad1fd1504b8580585ecd373588f8e8fe2339a1
Isaac Preyser <isaacpreyser@icloud.com>
2024-03-27 06:57:13 +0000

    lab done - maybe write in some iterative methods

--- Labs/Lab-09/RefBasedBinarySearchTree.java
+++ Labs/Lab-09/RefBasedBinarySearchTree.java
@@ -46,6 +46,52 @@ public class RefBasedBinarySearchTree extends RefBasedBinaryTree {
         }
     }
 
+  /* 
+     * Method name: sum 
+     * Purpose: computes the sum of all elements in this BinaryTree 
+     * Parameters: none 
+     * Returns: int – the sum 
+     */
+    //honestly, just perform a traversal and pass the sum along. 
+    public int sum() {
+        return sum(root);
+    }
+
+    private int sum(TreeNode node){
+        if (node == null) {
+            return 0;
+        }
+        return node.getValue() + sum(node.getLeft()) + sum(node.getRight());
+    }
+
+    public Boolean find(int value){
+        return find(root, value);
+    }
+
+    private Boolean find(TreeNode node, int value){
+        if (node == null) {
+            return false;
+        }
+        if (node.getValue() == value) {
+            return true;
+        }
+        return find(node.getLeft(), value) || find(node.getRight(), value); //if the value is found in either the left or the right subtree, return true. (percolate down until it is found. or return false. )
+    }
+
+    
+    public int getMax() throws TreeEmptyException {
+        if (root == null) {
+            throw new TreeEmptyException();
+        }
+        return getMax(root);
+    }
+
+    private int getMax(TreeNode node) {
+        if (node.getRight() == null) {
+            return node.getValue();
+        }
+        return getMax(node.getRight()); //recall that the rightmost node is the largest, following the BST property. 
+    }
 
     public static void main(String[] args) {
         // use these trees to test the methods you will implement
@@ -67,5 +113,23 @@ public class RefBasedBinarySearchTree extends RefBasedBinaryTree {
         myTree.postOrder();
         
         System.out.println("toString\n" + myTree);
+        System.out.println("sum: " + myTree.sum());
+
+        System.out.println("find 5: " + myTree.find(5));
+        System.out.println("find 3: " + myTree.find(3));
+
+        try {
+            System.out.println("max: " + myTree.getMax());
+        } catch (TreeEmptyException e) {
+            System.out.println("Tree is empty");
+        }
+
+        try {
+            System.out.println("max: " + emptyTree.getMax());
+        } catch (TreeEmptyException e) {
+            System.out.println("Tree is empty");
+        }
+
+
     }
 }
